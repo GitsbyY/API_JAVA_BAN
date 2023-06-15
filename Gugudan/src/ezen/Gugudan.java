a15fa5cbdb50ca8f449dfe1e7f3bd7ea18119e69
GitsbyY <dasseiende90@gmail.com>
2023-06-15 17:15:20 +0900

    구구단 2단을 했다

--- Gugudan/src/ezen/Gugudan.java
+++ Gugudan/src/ezen/Gugudan.java
@@ -7,8 +7,14 @@ public class Gugudan {
 
 		System.out.println("구구단 연습문제");
 		
-		System.out.println(2 * 1);
-		System.out.println(2 * 9);
+		int i,j; // i=단
+		for(i=2;i<=2;i++) {
+				
+			for(j=1;j<=9;j++) {
+				System.out.println(i+"X"+j+"="+(i*j));
+			}//안쪽 for끝
+			System.out.println("");//구분용 빈줄
+		}//바깥 for끝
 		
 	}
 
